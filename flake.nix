88e9d524504c0adfa3273605646ab034e088a104
Andreas Fehn <fehnomenal@fehn.systems>
2020-11-10 15:30:30 +0100

    Make it a flake

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+      devShell.x86_64-linux = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          openjdk11
+        ];
+      };
+    };
+}
