ab2a58c9706d08d6a0a6a78c85a654b2f937247e
Andreas Fehn <fehnomenal@fehn.systems>
2020-11-23 09:15:09 +0100

    Do not cache path to the executable

--- src/main/kotlin/systems/fehn/intellijdirenv/services/DirenvService.kt
+++ src/main/kotlin/systems/fehn/intellijdirenv/services/DirenvService.kt
@@ -6,20 +6,8 @@ import java.nio.file.Path
 import java.nio.file.Paths
 
 class DirenvService {
-    private var _direnvExecutable: Path? = null
-    var resolveDirenvExecutable = true
-
     val direnvExecutable: Path?
-        get() {
-            if (resolveDirenvExecutable) {
-                _direnvExecutable = resolveDirenvExecutable()
-                resolveDirenvExecutable = false
-            }
-            return _direnvExecutable
-        }
-
-    private fun resolveDirenvExecutable(): Path? =
-        System.getenv("PATH")
+        get() = System.getenv("PATH")
             .split(':')
             .asSequence()
             .map { Paths.get(it, "direnv") }
